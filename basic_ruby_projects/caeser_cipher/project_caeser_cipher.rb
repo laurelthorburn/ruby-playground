3cf17673404007db350929a9972ab78289867a9b
Laurel Thorburn <codinglaurel@gmail.com>
2022-05-17 06:42:09 -0600

    Fixed typo

---
+++ basic_ruby_projects/caeser_cipher/project_caeser_cipher.rb
@@ -0,0 +1,9 @@
+puts "Welcome to Caeser Cipher, enter a letter and a number, and I'll do the rest"
+
+=begin
+First, let's psuedo code.
+User gives a string and a number
+caeser_cipher("What a string!", 5)
+    => "Bmfy f xywnsl!"
+Tips: Convert string into a number, wrap z to a, keep same case
+=end

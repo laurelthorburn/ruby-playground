0a0bb9778a708764c4bfba2e3e433c409f4bbb13
Laurel Thorburn <codinglaurel@gmail.com>
2022-05-17 15:23:56 -0600

    Working on caesar cipher

--- basic_ruby_projects/caeser_cipher/project_caeser_cipher.rb
+++ basic_ruby_projects/caeser_cipher/project_caeser_cipher.rb
@@ -6,4 +6,25 @@ User gives a string and a number
 caeser_cipher("What a string!", 5)
     => "Bmfy f xywnsl!"
 Tips: Convert string into a number, wrap z to a, keep same case
+
+    1. split string .split('')
+
 =end
+
+# ! at the end of a method is called Bang
+# .ord and .char
+
+def caeser_cipher(string, number)
+    original_array = string.split('')
+    new_values = original_array.map do |letter|
+        (letter.ord % 26) + number
+    end
+    
+    new_msg = new_values.map do |value|
+        puts value #works
+        puts value.is_a? Integer #works
+        puts value.chr #prints empty string?
+    end
+end
+
+caeser_cipher("hello there", 5) 
